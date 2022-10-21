340d28f2b96f17d88f4e5d7337b386c69fb1f276
n-truong <n-truong@plott.co.jp>
2022-10-21 14:05:48 +0700

    test

---
+++ src/provider/TestServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hoangtruong1808\Test\provider;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/../routes/web.php';
+    }
+}
